8cd3c53e8ba0e95cd66c557fcccd8205638b08fe
cemugras/desktop <cemugrs@gmail.com>
2021-03-26 00:05:06 +0300

    Html test and debugging features added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[1].description;
+
+  price = 9.9569698;
+
+  rate = 0.67;
+
+  startDate = new Date(2000, 0, 1);
+
   /*coreCourse = COURSES[0];
   RxJsCourse = COURSES[1];
   NgRxCourse = COURSES[2];*/
